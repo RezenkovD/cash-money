e2feaa7edbbe7daf26caf0f21359f0c482a11be9
RezenkovD <rezenkovdmitro@gmail.com>
2023-03-16 09:32:11 +0200

    Add routers and tests for endpoints

--- src/routers/group.py
+++ src/routers/group.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter
 
@@ -30,3 +32,24 @@ def read_users_group(
     db: Session = Depends(get_db),
 ) -> schemas.UsersGroup:
     return services.read_users_group(db, group_id, current_user.id)
+
+
+@router.post(
+    "/{group_id}/leave/", response_model=Union[schemas.AboutUsers, schemas.UsersGroup]
+)
+def leave_group(
+    group_id: int,
+    current_user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> Union[schemas.AboutUsers, schemas.UsersGroup]:
+    return services.leave_group(db, group_id, current_user.id)
+
+
+@router.post("/{group_id}/remove/{user_id}", response_model=Union[schemas.AboutUsers, schemas.UsersGroup])
+def remove_user(
+    group_id: int,
+    user_id: int,
+    current_user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> Union[schemas.AboutUsers, schemas.UsersGroup]:
+    return services.remove_user(db, group_id, user_id, current_user.id)
