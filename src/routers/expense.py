547c81dfeb341ac1a8ed423f5bffc4b4544f1280
RezenkovD <rezenkovdmitro@gmail.com>
2023-03-24 17:55:24 +0200

    feat: add routers for read expenses by group

--- src/routers/expense.py
+++ src/routers/expense.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter
 
 from database import get_db
-from dependencies import get_current_user
+from dependencies import get_current_user, transform_date_or_422
 import models
 import schemas
 import services
@@ -22,3 +24,23 @@ def create_expense(
     db: Session = Depends(get_db),
 ) -> schemas.BaseExpense:
     return services.create_expense(db, group_id, expense, current_user.id)
+
+
+@router.get("/{group_id}/all-time/", response_model=List[schemas.UserExpense])
+def read_expenses_by_group_all_time(
+    group_id: int,
+    current_user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> List[schemas.UserExpense]:
+    return services.read_expenses_by_group(db, group_id, current_user.id)
+
+
+@router.get("/{group_id}/{year_month}/", response_model=List[schemas.UserExpense])
+def read_expenses_by_group_month(
+    year_month: str,
+    group_id: int,
+    current_user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> List[schemas.UserExpense]:
+    filter_date = transform_date_or_422(year_month)
+    return services.read_expenses_by_group(db, group_id, current_user.id, filter_date)
