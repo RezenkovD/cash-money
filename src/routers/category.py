8ed65d726c482bb5ebe6eddc4a6d231b060bd6da
RezenkovD <rezenkovdmitro@gmail.com>
2023-03-28 11:27:05 +0300

    perf: update all related to categories

--- src/routers/category.py
+++ src/routers/category.py
@@ -16,9 +16,10 @@ router = APIRouter(
 
 @router.post("/{group_id}/", response_model=schemas.Category)
 def create_category(
+    *,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user),
     group_id: int,
     category: schemas.CreateCategory,
-    current_user: models.User = Depends(get_current_user),
-    db: Session = Depends(get_db),
 ) -> schemas.Category:
-    return services.create_category(db, group_id, category, current_user.id)
+    return services.create_category(db, current_user.id, group_id, category)
--- tests/test_services/test_category.py
+++ tests/test_services/test_category.py
@@ -4,37 +4,45 @@ from starlette.exceptions import HTTPException
 import models
 from schemas import CreateCategory
 from services import create_category
-from tests.factories import UserFactory, GroupFactory, UserGroupFactory, CategoryFactory
+from tests.factories import UserFactory, GroupFactory, UserGroupFactory
 
 
 def test_create_category(session) -> None:
     user = UserFactory()
     group = GroupFactory(admin_id=user.id)
     UserGroupFactory(user_id=user.id, group_id=group.id)
-    category = CreateCategory(title="Book")
-    data = create_category(session, group.id, category, user.id)
+    category = CreateCategory(title="BoOK")
+    data = create_category(session, user.id, group.id, category)
     assert data.title == category.title.lower()
-
-    with pytest.raises(HTTPException) as ex_info:
-        create_category(session, group.id, category, user.id)
-    assert "The category is already in this group!" in str(ex_info.value.detail)
-
     db_category_group = session.query(models.CategoryGroups).all()
     assert len(db_category_group) == 1
 
-    second_category = CategoryFactory()
-    data = create_category(session, group.id, second_category, user.id)
-    assert data.title == second_category.title.lower()
 
-    db_category_group = session.query(models.CategoryGroups).all()
-    assert len(db_category_group) == 2
-
+def test_create_category_not_admin(session) -> None:
+    user = UserFactory()
+    group = GroupFactory(admin_id=user.id)
+    category = CreateCategory(title="Book")
     with pytest.raises(HTTPException) as ex_info:
-        create_category(session, group.id, category, 9999)
+        create_category(session, 9999, group.id, category)
     assert "You are not admin in this group!" in str(ex_info.value.detail)
 
+
+def test_create_category_inactive_group(session) -> None:
+    user = UserFactory()
     group = GroupFactory(admin_id=user.id, status=models.Status.INACTIVE)
     UserGroupFactory(user_id=user.id, group_id=group.id, status=models.Status.INACTIVE)
+    category = CreateCategory(title="Book")
     with pytest.raises(HTTPException) as ex_info:
-        create_category(session, group.id, category, user.id)
+        create_category(session, user.id, group.id, category)
     assert "Group is not active!" in str(ex_info.value.detail)
+
+
+def test_create_category_exist(session) -> None:
+    user = UserFactory()
+    group = GroupFactory(admin_id=user.id)
+    category = CreateCategory(title="BoOK")
+    create_category(session, user.id, group.id, category)
+    category = CreateCategory(title="BOoK")
+    with pytest.raises(HTTPException) as ex_info:
+        create_category(session, user.id, group.id, category)
+    assert "The category is already in this group!" in str(ex_info.value.detail)
