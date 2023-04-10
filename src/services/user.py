a4a156d124966562f8d5fc0799c2755c02feb79c
RezenkovD <rezenkovdmitro@gmail.com>
2023-04-10 16:42:13 +0300

    perf: update services and tests

--- src/services/user.py
+++ src/services/user.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from sqlalchemy.orm import Session
-from sqlalchemy.sql import func
+from sqlalchemy.sql.functions import coalesce, sum
 
 from models import User, Replenishment, Expense
 from schemas import CurrentBalance
@@ -12,24 +12,12 @@ def get_user(db: Session, login: str) -> Optional[User]:
 
 
 def read_user_current_balance(db: Session, user_id: int) -> CurrentBalance:
-    replenishments = (
-        db.query(func.sum(Replenishment.amount))
-        .filter_by(user_id=user_id)
-        .one_or_none()
-    )
-    if replenishments[0] is None:
-        replenishments = 0
-    else:
-        replenishments = replenishments[0]
-
-    expenses = (
-        db.query(func.sum(Expense.amount)).filter_by(user_id=user_id).one_or_none()
-    )
-    if expenses[0] is None:
-        expenses = 0
-    else:
-        expenses = expenses[0]
-
+    (replenishments,) = db.query(
+        coalesce(sum(Replenishment.amount).filter(Replenishment.user_id == user_id), 0)
+    ).one()
+    (expenses,) = db.query(
+        coalesce(sum(Expense.amount).filter(Expense.user_id == user_id), 0)
+    ).one()
     balance = replenishments - expenses
     balance = CurrentBalance(current_balance=balance)
     return balance
