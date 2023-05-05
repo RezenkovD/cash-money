a56f3d468c195ff15596e5963a27d37031679e2c
RezenkovD <rezenkovdmitro@gmail.com>
2023-05-05 16:52:41 +0300

    feat: add color, icon for categories

--- src/schemas/category.py
+++ src/schemas/category.py
@@ -3,7 +3,10 @@ from schemas.base_model import BaseModel
 
 class CreateCategory(BaseModel):
     title: str
+    icon_url: str
+    color_code: str
 
 
-class CategoryModel(CreateCategory):
+class CategoryModel(BaseModel):
     id: int
+    title: str
