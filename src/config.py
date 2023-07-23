2e9ccf94ac16de5cf121de9c0c3c81fbef5c2a4c
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-23 13:05:48 +0300

    feat: add permit cors policy

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     SERVER_METADATA_URL: str
     SQLALCHEMY_DATABASE_URI: str
+    DOMAIN_SOURCE: str
+    LOCAL_SOURCE: str
 
 
 settings = Settings()
