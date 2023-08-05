317acd5f281297a58a63bb872882ef9391808ed3
RezenkovD <rezenkovdmitro@gmail.com>
2023-08-05 16:31:19 +0300

    feat: add total-replenishments router

--- src/routers/user.py
+++ src/routers/user.py
@@ -13,7 +13,7 @@ from dependencies import (
     transform_exact_date_or_422,
 )
 from models import User
-from schemas import UserBalance, UserModel, UserTotalExpenses
+from schemas import UserBalance, UserModel, UserTotalExpenses, UserTotalReplenishments
 
 router = APIRouter(
     prefix="/users",
@@ -76,3 +76,39 @@ def read_user_total_expenses(
         )
     else:
         return services.user_total_expenses(db, current_user.id)
+
+
+@router.get("/total-replenishments/", response_model=UserTotalReplenishments)
+def read_user_total_expenses(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+    year_month: Optional[str] = None,
+    start_date: Optional[str] = None,
+    end_date: Optional[str] = None,
+) -> UserTotalReplenishments:
+    if year_month and (start_date or end_date):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Cannot use filter_date with start_date or end_date",
+        )
+    elif (start_date and not end_date) or (end_date and not start_date):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Both start_date and end_date are required",
+        )
+    elif year_month:
+        filter_date = transform_date_or_422(year_month)
+        return services.user_total_replenishments(
+            db, current_user.id, filter_date=filter_date
+        )
+    elif start_date and end_date:
+        start_date = transform_exact_date_or_422(start_date)
+        end_date = transform_exact_date_or_422(end_date)
+        return services.user_total_replenishments(
+            db,
+            current_user.id,
+            start_date=start_date,
+            end_date=end_date,
+        )
+    else:
+        return services.user_total_replenishments(db, current_user.id)
